a4a22fb5533be3ce5976f22eeae367bf7d7ca106
SolKul <46105598+SolKul@users.noreply.github.com>
2020-12-26 22:45:45 +0900

    update main

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from md_parser import md_parser
 
 def main():
-    md_path=Path("Hydrogen.md")
+    md_path=Path("Sample.md")
     md_parser.parse_md_to_hatena(md_path)
 
 if __name__ == "__main__":
